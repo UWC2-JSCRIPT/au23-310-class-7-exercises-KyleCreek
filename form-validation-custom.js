2be1cac26035332fcc8da461f8bddf21629d2225
KyleCreek <kyle.a.creek@gmail.com>
2023-11-23 09:07:31 -0800

    Completed 'form-validation-custom'

--- form-validation-custom.js
+++ form-validation-custom.js
@@ -10,19 +10,23 @@ const employees = document.getElementById("num-of-employees");
 
 
 const validLength = (input, min) => {
-    if (input.value.trim().length > min){
+    if (input.value.trim().length >= min){
         input.parentElement.classList.remove("invalid");
+        return true;
     } else {
         input.parentElement.classList.add("invalid");
+        return false;
     }
 };
 
 const validateEmail = (emailField) => {
     const re = /\w+@\w+\.\w+/
-    if (re.test(emailField.value.time())){
+    if (re.test(emailField.value.trim())){
         emailField.parentElement.classList.remove("invalid");
+        return true;
     } else {
         emailField.parentElement.classList.add('invalid');
+        return false;
     }
 };
 
@@ -44,15 +48,20 @@ select.addEventListener("change", handleSelect(select));
 
 
 form.addEventListener("submit", (e) => {
+    // Set a Flag to Determine if we want to actually Submit the Form. 
+    let fName = false;
+    let lName = false;
+    let eBool = false;
 
+    // Validate First and Last Name are Three Characters or More
+    fname = validLength(firstName, 3);
+    lname = validLength(lastName, 3);
+    eBool = validateEmail(email);
 
-    // Only Prevent DEfault if all are valid or invalid. 
-    // VAlid case Statement
-    // Invalid Case Statement
-    //e.preventDefault()
-    
-    
-    validLength(firstName, 3);
-    validLength(lastName, 3);
-    //validateEmail(email);
+    // If any item is not valid, preventDefault() on the Submit Listener and log 
+    // 'Bad Input' to the Console.
+    if (fname == false || lanem == false || eBool == false){
+        e.preventDefault();
+        console.log('Bad Input');
+    }
 });
